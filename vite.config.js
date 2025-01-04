20d9fb6de105ee1456761cd7e0657f9c683b2364
ABHAY VAGHELA <165791471+abhayvaghela01@users.noreply.github.com>
2025-01-04 20:15:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+   base: '/bbc-news-clone/',
   css: {
     postcss: "./postcss.config.cjs" // Ensure this matches your PostCSS file name
   }
